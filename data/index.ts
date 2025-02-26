300c835340fcef8a56c50f412a49d09d07229f23
Driton Gashi <dritongashi1995@gmail.com>
2025-02-26 23:59:12 +0100

    fix(navbar): added the navigations

--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,5 @@
 export const navItems = [
+  { name: "Home", link: "#top" },
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
